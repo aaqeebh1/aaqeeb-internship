6e3249edc9890d81dbef7fcb973e99c4d1054f43
Aaqeeb <24283167+aaqeebh1@users.noreply.github.com>
2022-11-08 01:28:00 +0000

    Dynamic Explore page

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -6,6 +6,7 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import axios from "axios";
 import CountdownTimer from "./CountDownTimer";
+import Skeleton from "../UI/Skeleton";
 
 const NewItems = () => {
   const baseUrl =
@@ -62,56 +63,9 @@ const NewItems = () => {
           </div>
           <OwlCarousel className="slider-items owl-carousel" {...options}>
             {!loading
-              ? new Array(4).fill(0).map((_, index) => (
-                  <div className="" key={index}>
-                    <div className="nft__item">
-                      <div className="author_list_pp--loading">
-                        <Link to="/author">
-                          <div className="pp-coll_loading" alt=""></div>
-                        </Link>
-                        <i className="fa fa-check"></i>
-                      </div>
-                      <div className=" de_countdown--loading"></div>
-
-                      <div className="nft__item_wrap">
-                        <div className="nft__item_extra">
-                          <div className="nft__item_buttons">
-                            <button>Buy Now</button>
-                            <div className="nft__item_share">
-                              <h4>Share</h4>
-                              <a
-                                href="https://www.facebook.com/sharer/sharer.php?u=https://gigaland.io"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                <i className="fa fa-facebook fa-lg"></i>
-                              </a>
-                              <a
-                                href="https://twitter.com/intent/tweet?url=https://gigaland.io"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                <i className="fa fa-twitter fa-lg"></i>
-                              </a>
-                              <a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://gigaland.io">
-                                <i className="fa fa-envelope fa-lg"></i>
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-
-                        <Link to="/item-details">
-                          <div className="lazy nft__item_preview--loading"></div>
-                        </Link>
-                      </div>
-                      <div className="nft__item_info">
-                        <div className="nft_coll_info-title-loading"></div>
-                        <div className="nft_coll_info-description-loading"></div>
-                      </div>
-                    </div>
-                  </div>
-                ))
+            ? <Skeleton itemNo={4}/>
               : post?.map((post, index) => (
+                
                   <div className="" key={index}>
                     <div className="nft__item">
                       <div className="author_list_pp">
@@ -182,7 +136,7 @@ const NewItems = () => {
                     </div>
                   </div>
                 ))}
-          </OwlCarousel>
+                </OwlCarousel>
         </div>
       </div>
     </section>
